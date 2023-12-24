c736a4e900d05521251f11fd9e67356253c717e0
@sw_mypro <2792069459@qq.com>
2023-12-24 15:24:49 +0800

    进程状态

---
+++ lesson13/test.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+
+int main()
+{
+
+    while(1)
+    {
+        printf("hello linux!\n");
+    }
+    //int a = 0;
+    //printf("please Enter #\n");
+    //scanf("%d",&a);
+    //printf("a is : %d\n",a);
+    return 0;
+}

7b8f7e0bcb2e3fca88c6424c8d366a8e07666c31
@sw_mypro <2792069459@qq.com>
2024-02-15 15:55:43 +0800

    命名管道

---
+++ lesson30/fifo/client.cc
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<sys/types.h>
+#include<sys/stat.h>
+#include<unistd.h>
+#include<cerrno>
+#include<cstdlib>
+#include<cstring>
+#include<fcntl.h>
+#include"comm.h"
+using namespace std;
+
+int main()
+{
+    int wfd = open(FILENAME,O_WRONLY);
+    if(wfd<0)
+    {
+        cerr<<"errno: "<<errno<<", errstring: "<<strerror(errno)<<endl;
+        return 1;
+    }
+    
+    cout<<"mkfifo success ...write"<<endl;
+    string message;
+    while(true)
+    {
+        cout<<"Please Enter# ";
+        getline(cin,message);
+
+        ssize_t s = write(wfd,message.c_str(),message.size());
+        if(s<0)
+        {
+            cerr<<"errno: "<<errno<<", errstring: "<<strerror(errno)<<endl;
+            break;
+        }
+    }
+
+    close(wfd);
+    cout<<"close fifo success ..."<<endl;
+    return 0;
+}

d4b7afb3b3c9ad9285475e71ae0f4a32e9146546
孙文超 <2792069459@qq.com>
2024-03-26 22:34:36 +0800

    线程互斥

--- lesson39/main.cc
+++ lesson39/main.cc
@@ -20,12 +20,60 @@ void Print(int num)
     }
 }
 
+int ticket = 10000;//全局的共享资源
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+
+
+void GetTicket(std::string name)
+{
+    while(true)
+    {
+        pthread_mutex_lock(&mutex);
+        if(ticket>0)
+        {
+            usleep(1000);
+            printf("%s get a ticket: %d\n",name.c_str(),ticket);
+            ticket--;
+            pthread_mutex_unlock(&mutex);
+
+        }
+        else 
+        {
+            pthread_mutex_unlock(&mutex);
+            break;
+        }
+    }
+}
+
 int main()
 {
-    Thread<int> t(GetThreadName(),Print,10);
-    std::cout<<"thread is running? "<<t.IsRunning()<<std::endl;
-    t.Start();
-    std::cout<<"thread is running? "<<t.IsRunning()<<std::endl;
-    t.Join();
+    std::string name1 = GetThreadName();
+    Thread<std::string>t1(name1,GetTicket,name1);
+
+    std::string name2 = GetThreadName();
+    Thread<std::string>t2(name2,GetTicket,name2);
+
+    std::string name3 = GetThreadName();
+    Thread<std::string>t3(name3,GetTicket,name3);
+
+    std::string name4 = GetThreadName();
+    Thread<std::string>t4(name4,GetTicket,name4);
+
+    t1.Start();
+    t2.Start();
+    t3.Start();
+    t4.Start();
+
+    t1.Join();
+    t2.Join();
+    t3.Join();
+    t4.Join();
+
+
+    // Thread<int> t(GetThreadName(),Print,10);
+    // std::cout<<"thread is running? "<<t.IsRunning()<<std::endl;
+    // t.Start();
+    // std::cout<<"thread is running? "<<t.IsRunning()<<std::endl;
+    // t.Join();
     return 0;
 }
